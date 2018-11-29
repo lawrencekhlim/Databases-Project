23b4d4518c7b689293eb001d17b772e2cc29602d
lawrencekhlim <lawrencekhlim@gmail.com>
2018-11-28 22:01:06 -0800

    Updates to BackTellerInterface.java for better UI

--- BankTellerInterface.java
+++ BankTellerInterface.java
@@ -9,6 +9,22 @@ public class BankTellerInterface extends JFrame implements ActionListener{
     JPanel enterCheckPanel;
     JPanel createAcctPanel;
     JPanel delTransPanel;
+    
+    JButton checkTransButton;
+    JButton genMSButton;
+    JButton listClosedButton;
+    JButton genDTERButton;
+    JButton customerReportButton;
+    JButton addInterestButton;
+    JButton createAcctButton;
+    JButton delAcctButton;
+    JButton delTransButton;
+    
+    JTextField accountNumberTextField1;
+    JTextField enterCheckTextField1;
+    
+    JTextField enterCustomerTextField2;
+    
 
 	public BankTellerInterface() {
         initUI();
@@ -42,43 +58,94 @@ public class BankTellerInterface extends JFrame implements ActionListener{
         //_-----------------------------------------------------------
 
         //enterCheckPanel --------------------------------------------
+        
         enterCheckPanel = new JPanel();
         enterCheckPanel.setLayout(new FlowLayout());
+        /*
         enterCheckPanel.setOpaque(false);
         enterCheckPanel.setBackground(Color.WHITE);
         enterCheckPanel.setSize(150,150);
-
-        JLabel enterCheckLabel  = new JLabel();
-        enterCheckLabel.setText("Enter Check Values:");
-
-        enterCheckPanel.add(enterCheckLabel);
+        */
+        JButton cancel1 = new JButton ("Cancel");
+        cancel1.addActionListener ( new ActionListener (){
+            public void actionPerformed (ActionEvent e) {
+                idleView();
+            }
+        });
+        JLabel accountNumberLabel1 = new JLabel ("Account Number");
+        accountNumberTextField1 = new JTextField ();
+        accountNumberTextField1.setPreferredSize(new Dimension(150,25));
+        
+        JLabel enterCheckLabel1  = new JLabel();
+        enterCheckLabel1.setText("Check Value: $");
+        enterCheckTextField1 = new JTextField();
+        enterCheckTextField1.setPreferredSize(new Dimension(150,25));
+        
+        JButton submit1 = new JButton ("Submit");
+        submit1.addActionListener ( new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                idleView();
+            }
+        });
+        
+        enterCheckPanel.add(cancel1);
+        enterCheckPanel.add(accountNumberLabel1);
+        enterCheckPanel.add(accountNumberTextField1);
+        enterCheckPanel.add(enterCheckLabel1);
+        enterCheckPanel.add(enterCheckTextField1);
+        enterCheckPanel.add(submit1);
+        
         //end enter check panel-----------------------------------------
 
-        //createAcctPanrl --------------------------------------------
+        //createAcctPanel --------------------------------------------
+        
         createAcctPanel = new JPanel();
         createAcctPanel.setLayout(new FlowLayout());
+        /*
         createAcctPanel.setOpaque(false);
         createAcctPanel.setBackground(Color.WHITE);
         createAcctPanel.setSize(150,150);
-
-        JLabel createAcctLabel  = new JLabel();
-        createAcctLabel.setText("Create Account:");
-
-        enterCheckPanel.add(createAcctLabel);
+        */
+        JButton cancel2 = new JButton ("Cancel");
+        cancel2.addActionListener ( new ActionListener (){
+            public void actionPerformed (ActionEvent e) {
+                idleView();
+            }
+        });
+        
+        JLabel createAcctLabel2  = new JLabel();
+        createAcctLabel2.setText("Customer TID");
+        enterCustomerTextField2 = new JTextField();
+        enterCustomerTextField2.setPreferredSize(new Dimension(150,25));
+        JButton submit2 = new JButton ("Submit");
+        submit2.addActionListener ( new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                idleView();
+            }
+        });
+        createAcctPanel.add(cancel2);
+        createAcctPanel.add(createAcctLabel2);
+        createAcctPanel.add(enterCustomerTextField2);
+        createAcctPanel.add(submit2);
         //end create acct panel-----------------------------------------
 
-        //createAcctPanrl --------------------------------------------
+        
+        //delTransPanrl --------------------------------------------
+        
         delTransPanel = new JPanel();
         delTransPanel.setLayout(new FlowLayout());
+        /*
         delTransPanel.setOpaque(false);
         delTransPanel.setBackground(Color.WHITE);
         delTransPanel.setSize(150,150);
-
+        */
+        
         JLabel delTransLabel  = new JLabel();
         delTransLabel.setText("Delete Transaction:");
 
         delTransPanel.add(delTransLabel);
-        //end create acct panel-----------------------------------------
+        
+        //delTransPanel-----------------------------------------
 
 
 
@@ -105,59 +172,119 @@ public class BankTellerInterface extends JFrame implements ActionListener{
         gridButtons.setVisible(true);
 
 
-        JButton checkTransButton = new JButton();
+        checkTransButton = new JButton();
         checkTransButton.setText("Enter Check Transaction");
         gridButtons.add(checkTransButton);
         checkTransButton.addActionListener (new ActionListener () {
             public void actionPerformed (ActionEvent e) {
                 ((CardLayout)userInterface.getLayout()).show (userInterface, "enterCheckPanel");
-                //actionStatus = 4;
-                System.out.println("here");
+                setGridButtonsVisible (false);
+                checkTransButton.setVisible (true);
             }
         });
 
-        JButton genMSButton = new JButton();
+        genMSButton = new JButton();
         genMSButton.setText("Generate Monthly Statement");
         gridButtons.add(genMSButton);
+        genMSButton.addActionListener (new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
 
-        JButton listClosedButton = new JButton();
+            }
+        });
+
+        listClosedButton = new JButton();
         listClosedButton.setText("List Closed Accounts");
         gridButtons.add(listClosedButton);
+        listClosedButton.addActionListener (new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                
+            }
+        });
 
-        JButton genDTERButton = new JButton();
+        genDTERButton = new JButton();
         genDTERButton.setText("Generate DTER");
         gridButtons.add(genDTERButton);
+        genDTERButton.addActionListener (new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                
+            }
+        });
 
-        JButton customerReportButton = new JButton();
+        customerReportButton = new JButton();
         customerReportButton.setText("Customer Report");
         gridButtons.add(customerReportButton);
+        customerReportButton.addActionListener(new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                
+            }
+        });
 
-        JButton addInterestButton = new JButton();
+        addInterestButton = new JButton();
         addInterestButton.setText("Add Interest");
         gridButtons.add(addInterestButton);
+        addInterestButton.addActionListener(new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                
+            }
+        });
 
-        JButton createAcctButton = new JButton();
+        createAcctButton = new JButton();
         createAcctButton.setText("Create Account");
         gridButtons.add(createAcctButton);
+        createAcctButton.addActionListener(new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                ((CardLayout)userInterface.getLayout()).show (userInterface, "createAcctPanel");
+                setGridButtonsVisible (false);
+                createAcctButton.setVisible (true);
+            }
+        });
 
-        JButton delAcctButton = new JButton();
+        delAcctButton = new JButton();
         delAcctButton.setText("Delete Closed Accounts and Customers");
         gridButtons.add(delAcctButton);
+        delAcctButton.addActionListener(new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                
+            }
+        });
 
-        JButton delTransButton = new JButton();
+        delTransButton = new JButton();
         delTransButton.setText("Delete Transactions");
-        gridButtons.add(delTransButton); 
+        gridButtons.add(delTransButton);
+        delTransButton.addActionListener(new ActionListener () {
+            public void actionPerformed (ActionEvent e) {
+                
+            }
+        });
         //end grid buttons
 
         getContentPane().add(title);
+        getContentPane().add(userInterface);
         getContentPane().add(gridButtons);
-        getContentPane().add(screen);
   
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-		public void actionPerformed(ActionEvent e){
-				System.out.println("Oui!");
-		}
+    public void actionPerformed(ActionEvent e){
+            System.out.println("Oui!");
+    }
+    
+    public void setGridButtonsVisible (boolean visible) {
+        checkTransButton.setVisible (visible);
+        genMSButton.setVisible (visible);
+        listClosedButton.setVisible (visible);
+        genDTERButton.setVisible (visible);
+        customerReportButton.setVisible (visible);
+        addInterestButton.setVisible (visible);
+        createAcctButton.setVisible (visible);
+        delAcctButton.setVisible (visible);
+        delTransButton.setVisible (visible);
+    }
+    
+    public void idleView () {
+        ((CardLayout)userInterface.getLayout()).show (userInterface, "idleState");
+        setGridButtonsVisible(true);
+    }
+    
     public static void main(String[] args) {
         BankTellerInterface ex = new BankTellerInterface();
         ex.setVisible(true);
